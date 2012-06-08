699abb1bca9d71d2a516cf7e7e5c63dfcee67de3
Jan Dolecek <juzna.cz@gmail.com>
2012-06-08 15:22:27 +0200

    base of parser definition

---
+++ src/cz/juzna/intellij/neon/parser/NeonParserDefinition.java
@@ -0,0 +1,75 @@
+package cz.juzna.intellij.neon.parser;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.ParserDefinition;
+import com.intellij.lang.PsiParser;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.IFileElementType;
+import com.intellij.psi.tree.TokenSet;
+import cz.juzna.intellij.neon.NeonElementTypes;
+import cz.juzna.intellij.neon.lexer.NeonLexer;
+import cz.juzna.intellij.neon.lexer.NeonTokenTypes;
+import cz.juzna.intellij.neon.psi.impl.NeonFileImpl;
+import cz.juzna.intellij.neon.psi.impl.NeonPsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class NeonParserDefinition implements ParserDefinition {
+    @NotNull
+    @Override
+    public Lexer createLexer(Project project) {
+        return new NeonLexer();
+    }
+
+    @Override
+    public PsiParser createParser(Project project) {
+        return new NeonParser();
+    }
+
+    @Override
+    public IFileElementType getFileNodeType() {
+        return NeonElementTypes.FILE;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getWhitespaceTokens() {
+        return NeonTokenTypes.WHITESPACES;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getCommentTokens() {
+        return NeonTokenTypes.COMMENTS;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getStringLiteralElements() {
+        return NeonTokenTypes.STRING_LITERALS;
+    }
+
+    @NotNull
+    @Override
+    public PsiElement createElement(ASTNode node) {
+	    IElementType type = node.getElementType();
+
+	    /*if(type == NeonTokenTypes.MACRO_NODE) return new MacroNodeImpl(node);
+	    else if(type == NeonTokenTypes.MACRO_ATTR) return new MacroAttrImpl(node);
+	    else*/ return new NeonPsiElement(node);
+    }
+
+    @Override
+    public PsiFile createFile(FileViewProvider fileViewProvider) {
+        return new NeonFileImpl(fileViewProvider);
+    }
+
+    @Override
+    public SpaceRequirements spaceExistanceTypeBetweenTokens(ASTNode astNode, ASTNode astNode1) {
+        return SpaceRequirements.MAY;
+    }
+}
