cc9b9bc18aad2b1d8d366ad67f2c7608238c7a03
Jan Dolecek <juzna.cz@gmail.com>
2012-06-10 12:40:53 +0200

    wip: parser

--- src/main/java/cz/juzna/intellij/neon/parser/NeonElementTypes.java
+++ src/main/java/cz/juzna/intellij/neon/parser/NeonElementTypes.java
@@ -13,18 +13,21 @@ import cz.juzna.intellij.neon.lexer.NeonTokenTypes;
 public interface NeonElementTypes {
 	public static final IFileElementType FILE = new IFileElementType(NeonLanguage.LANGUAGE);
 
+	public static final NeonElementType DOCUMENT = new NeonElementType("Document");
 	public static final NeonElementType KEY_VALUE_PAIR = new NeonElementType("Key value pair");
 	public static final NeonElementType HASH = new NeonElementType("Hash");
 	public static final NeonElementType ARRAY = new NeonElementType("Array");
+	public static final NeonElementType ARGS = new NeonElementType("Args");
 	public static final NeonElementType SEQUENCE = new NeonElementType("Sequence");
 	public static final NeonElementType COMPOUND_VALUE = new NeonElementType("Compound value");
 	public static final NeonElementType SCALAR_LIST_VALUE = new NeonElementType("Scalar list value");
 	public static final NeonElementType SCALAR_TEXT_VALUE = new NeonElementType("Scalar text value");
 
 	public static final TokenSet SCALAR_VALUES = TokenSet.create(
-		// TODO: add more
 		NeonTokenTypes.NEON_STRING,
 		NeonTokenTypes.NEON_NUMBER,
-		SCALAR_LIST_VALUE
+		NeonTokenTypes.NEON_REFERENCE,
+		NeonTokenTypes.NEON_IDENTIFIER,
+		NeonTokenTypes.NEON_LITERAL
 	);
 }
--- src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
+++ src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
@@ -11,23 +11,154 @@ import org.jetbrains.annotations.NotNull;
  * Neon parser, convert tokens (output from lexer) into syntax tree
  */
 public class NeonParser implements PsiParser, NeonTokenTypes, NeonElementTypes {
+	private PsiBuilder myBuilder;
+	private boolean eolSeen = false;
+	private int myIndent;
+	private boolean myHasTabs = false; // FIXME: use this
+	private PsiBuilder.Marker myAfterLastEolMarker;
+
+
 	@NotNull
 	@Override
 	public ASTNode parse(IElementType root, PsiBuilder builder) {
-		final PsiBuilder.Marker rootMarker = builder.mark();
+		builder.setDebugMode(true);
 
-		while (!builder.eof()) {
-			IElementType token = builder.getTokenType();
+		myBuilder = builder;
+		myIndent = 0;
+		eolSeen = false;
 
-			if (token != null) {
-				builder.mark().done(token);
-			}
+		// begin
+		PsiBuilder.Marker fileMarker = mark();
 
-			builder.advanceLexer();
-		}
+		passEmpty(); // process beginning of file
+		parseStatements(0);
+		assert (this.myBuilder.eof()) : "Not all tokens were passed.";
 
-		rootMarker.done(root);
+		// end
+		fileMarker.done(root);
 		return builder.getTreeBuilt();
 	}
 
+
+	private void parseStatements(int indent) {
+		while (!myBuilder.eof() && (myIndent >= indent)) {
+			parseStatement(indent);
+		}
+	}
+
+	private void parseStatement(int indent) {
+		if (myBuilder.eof()) return;
+
+		IElementType type = myBuilder.getTokenType();
+		if(type == NEON_KEY) parseKeyVal(indent);
+		else if (type == NEON_LPAREN) parseArguments();
+		else if(type == NEON_LBRACE_SQUARE) parseArray();
+		else if(type == NEON_LBRACE_CURLY) parseHash();
+		else advanceLexer(); // not known
+
+		passEmpty();
+	}
+
+
+	// arguments in parenthesis
+	private void parseArguments() {
+		PsiBuilder.Marker marker = mark();
+		while (myBuilder.getTokenType() != NEON_RPAREN) {
+			// TODO: recognize
+			advanceLexer();
+		}
+		marker.done(ARGS);
+	}
+
+	private void parseHash() {
+		PsiBuilder.Marker marker = mark();
+		while (myBuilder.getTokenType() != NEON_RBRACE_CURLY) {
+			parseKeyVal(myIndent);
+			passEmpty();
+		}
+		advanceLexer();
+		marker.done(HASH);
+	}
+
+	private void parseArray() {
+		PsiBuilder.Marker marker = mark();
+		while (myBuilder.getTokenType() != NEON_RBRACE_SQUARE) {
+			parseValue();
+			passEmpty();
+		}
+		advanceLexer();
+		marker.done(SEQUENCE);
+	}
+
+	private void parseKeyVal(int indent) {
+		assert myBuilder.getTokenType() == NEON_KEY;
+
+		PsiBuilder.Marker keyValPair = mark();
+		eolSeen = false;
+
+		// key colon value
+		advanceLexer();
+		assert myBuilder.getTokenType() == NEON_ASSIGNMENT;
+
+		advanceLexer(); passEmpty();
+		parseValue();
+
+		keyValPair.done(KEY_VALUE_PAIR);
+	}
+
+	private void parseValue() {
+		if (SCALAR_VALUES.contains(myBuilder.getTokenType())) {
+			PsiBuilder.Marker val = mark();
+			advanceLexer();
+			val.done(SCALAR_TEXT_VALUE);
+		} else {
+			PsiBuilder.Marker val = mark();
+			parseStatements(myIndent);
+			val.done(COMPOUND_VALUE);
+		}
+	}
+
+
+	/***  helpers ***/
+
+	private void advanceLexer() {
+		if (this.myBuilder.eof()) return;
+
+		IElementType type = this.myBuilder.getTokenType();
+		this.eolSeen = this.eolSeen || type == NEON_INDENT;
+		if (type == NEON_INDENT) {
+//			this.myAfterLastEolMarker = mark();
+			this.myIndent = this.myBuilder.getTokenText().length() - 1;
+		}
+		else {
+//			dropEolMarker();
+		}
+		this.myBuilder.advanceLexer();
+	}
+
+	private void dropEolMarker() {
+		if (this.myAfterLastEolMarker != null) {
+			this.myAfterLastEolMarker.drop();
+			this.myAfterLastEolMarker = null;
+		}
+	}
+
+	private void rollBackToEol() {
+		if ((this.eolSeen) && (this.myAfterLastEolMarker != null)) {
+			this.eolSeen = false;
+			this.myAfterLastEolMarker.rollbackTo();
+			this.myAfterLastEolMarker = null;
+		}
+	}
+
+	private PsiBuilder.Marker mark() {
+		dropEolMarker();
+		return this.myBuilder.mark();
+	}
+
+	private void passEmpty() {
+		while (!myBuilder.eof() && (myBuilder.getTokenType() == NEON_INDENT || myBuilder.getTokenType() == NEON_WHITESPACE)) {
+			advanceLexer();
+		}
+	}
 }
