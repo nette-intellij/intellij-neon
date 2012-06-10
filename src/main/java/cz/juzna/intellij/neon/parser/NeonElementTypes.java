850e9d32d0ce83d319150d874b610fa32b9d0619
Jan Dolecek <juzna.cz@gmail.com>
2012-06-10 15:48:49 +0200

    parser looks much better

--- src/main/java/cz/juzna/intellij/neon/parser/NeonElementTypes.java
+++ src/main/java/cz/juzna/intellij/neon/parser/NeonElementTypes.java
@@ -13,15 +13,15 @@ import cz.juzna.intellij.neon.lexer.NeonTokenTypes;
 public interface NeonElementTypes {
 	public static final IFileElementType FILE = new IFileElementType(NeonLanguage.LANGUAGE);
 
-	public static final NeonElementType DOCUMENT = new NeonElementType("Document");
 	public static final NeonElementType KEY_VALUE_PAIR = new NeonElementType("Key value pair");
 	public static final NeonElementType HASH = new NeonElementType("Hash");
+	public static final NeonElementType ITEM = new NeonElementType("Item");
+	public static final NeonElementType ENTITY = new NeonElementType("Entity");
 	public static final NeonElementType ARRAY = new NeonElementType("Array");
 	public static final NeonElementType ARGS = new NeonElementType("Args");
 	public static final NeonElementType SEQUENCE = new NeonElementType("Sequence");
 	public static final NeonElementType COMPOUND_VALUE = new NeonElementType("Compound value");
-	public static final NeonElementType SCALAR_LIST_VALUE = new NeonElementType("Scalar list value");
-	public static final NeonElementType SCALAR_TEXT_VALUE = new NeonElementType("Scalar text value");
+	public static final NeonElementType SCALAR_VALUE = new NeonElementType("Scalar value");
 
 	public static final TokenSet SCALAR_VALUES = TokenSet.create(
 		NeonTokenTypes.NEON_STRING,
--- src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
+++ src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
@@ -51,10 +51,7 @@ public class NeonParser implements PsiParser, NeonTokenTypes, NeonElementTypes {
 
 		IElementType type = myBuilder.getTokenType();
 		if(type == NEON_KEY) parseKeyVal(indent);
-		else if (type == NEON_LPAREN) parseArguments();
-		else if(type == NEON_LBRACE_SQUARE) parseArray();
-		else if(type == NEON_LBRACE_CURLY) parseHash();
-		else advanceLexer(); // not known
+		else parseValue(indent);
 
 		passEmpty();
 	}
@@ -62,60 +59,109 @@ public class NeonParser implements PsiParser, NeonTokenTypes, NeonElementTypes {
 
 	// arguments in parenthesis
 	private void parseArguments() {
+		assert myBuilder.getTokenType() == NEON_LPAREN : "Expected left parenthesis";
+
 		PsiBuilder.Marker marker = mark();
+		advanceLexer();
 		while (myBuilder.getTokenType() != NEON_RPAREN) {
-			// TODO: recognize
-			advanceLexer();
+			parseValue(0);
+			if (myBuilder.getTokenType() == NEON_ITEM_DELIMITER) advanceLexer();
 		}
+		advanceLexer();
 		marker.done(ARGS);
 	}
 
 	private void parseHash() {
+		assert myBuilder.getTokenType() == NEON_LBRACE_CURLY : "Expected left curly bracket";
+
 		PsiBuilder.Marker marker = mark();
+		advanceLexer();
 		while (myBuilder.getTokenType() != NEON_RBRACE_CURLY) {
 			parseKeyVal(myIndent);
-			passEmpty();
+			if (myBuilder.getTokenType() == NEON_ITEM_DELIMITER) advanceLexer();
 		}
 		advanceLexer();
 		marker.done(HASH);
 	}
 
 	private void parseArray() {
+		assert myBuilder.getTokenType() == NEON_LBRACE_CURLY : "Expected left square bracket";
+
 		PsiBuilder.Marker marker = mark();
+		advanceLexer();
 		while (myBuilder.getTokenType() != NEON_RBRACE_SQUARE) {
-			parseValue();
-			passEmpty();
+			parseValue(0);
+			if (myBuilder.getTokenType() == NEON_ITEM_DELIMITER) advanceLexer();
 		}
 		advanceLexer();
-		marker.done(SEQUENCE);
+		marker.done(ARRAY);
 	}
 
 	private void parseKeyVal(int indent) {
-		assert myBuilder.getTokenType() == NEON_KEY;
+		assert myBuilder.getTokenType() == NEON_KEY : "Expected key";
 
 		PsiBuilder.Marker keyValPair = mark();
+		advanceLexer();
 		eolSeen = false;
 
 		// key colon value
+		assert myBuilder.getTokenType() == NEON_ASSIGNMENT : "Expected assignment operator";
 		advanceLexer();
-		assert myBuilder.getTokenType() == NEON_ASSIGNMENT;
 
-		advanceLexer(); passEmpty();
-		parseValue();
+		// value
+		if (myBuilder.getTokenType() == NEON_INDENT) {
+			advanceLexer(); // read indent
+			PsiBuilder.Marker val = mark();
+			parseStatements(myIndent);
+			val.done(COMPOUND_VALUE);
+		} else {
+			parseValue(indent);
+		}
 
 		keyValPair.done(KEY_VALUE_PAIR);
 	}
 
-	private void parseValue() {
-		if (SCALAR_VALUES.contains(myBuilder.getTokenType())) {
+	private void parseSequence(int indent) {
+		assert myBuilder.getTokenType() == NEON_ARRAY_BULLET : "Expected array bullet";
+		PsiBuilder.Marker sequence = mark();
+
+		while (myIndent >= indent && myBuilder.getTokenType() == NEON_ARRAY_BULLET) {
+			PsiBuilder.Marker item = mark();
+			advanceLexer();
+			parseStatement(indent + 1);
+			passEmpty();
+			item.done(ITEM);
+		}
+
+		sequence.done(SEQUENCE);
+	}
+
+
+	private void parseValue(int indent) {
+		IElementType type = myBuilder.getTokenType();
+
+		if (SCALAR_VALUES.contains(type)) {
 			PsiBuilder.Marker val = mark();
 			advanceLexer();
-			val.done(SCALAR_TEXT_VALUE);
+
+			if (myBuilder.getTokenType() == NEON_LPAREN) {
+				parseArguments();
+				val.done(ENTITY);
+			} else {
+				val.done(SCALAR_VALUE);
+			}
 		} else {
-			PsiBuilder.Marker val = mark();
-			parseStatements(myIndent);
-			val.done(COMPOUND_VALUE);
+			parseCompoundValue(indent);
 		}
+	}
+
+	private void parseCompoundValue(int indent) {
+		IElementType type = myBuilder.getTokenType();
+
+		if(type == NEON_LBRACE_SQUARE) parseArray();
+		else if(type == NEON_LBRACE_CURLY) parseHash();
+		else if(type == NEON_ARRAY_BULLET) parseSequence(myIndent);
+		else advanceLexer(); // not known
 	}
 
 
@@ -157,7 +203,7 @@ public class NeonParser implements PsiParser, NeonTokenTypes, NeonElementTypes {
 	}
 
 	private void passEmpty() {
-		while (!myBuilder.eof() && (myBuilder.getTokenType() == NEON_INDENT || myBuilder.getTokenType() == NEON_WHITESPACE)) {
+		while (!myBuilder.eof() && (myBuilder.getTokenType() == NEON_INDENT || myBuilder.getTokenType() == NEON_UNKNOWN)) {
 			advanceLexer();
 		}
 	}
