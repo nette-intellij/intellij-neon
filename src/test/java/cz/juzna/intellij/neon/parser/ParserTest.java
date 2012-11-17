0896a0e2365315f54d3b6dd782352fd4d1c00787
Martin Stekl <martin.stekl@gmail.com>
2012-11-17 21:06:00 +0100

    made ParserTest works and update test data

--- src/test/java/cz/juzna/intellij/neon/parser/ParserTest.java
+++ src/test/java/cz/juzna/intellij/neon/parser/ParserTest.java
@@ -1,27 +1,15 @@
 package cz.juzna.intellij.neon.parser;
 
-import com.intellij.lang.LanguageASTFactory;
-import com.intellij.lang.ParserDefinition;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.ParsingTestCase;
 import com.intellij.testFramework.PlatformTestCase;
-import cz.juzna.intellij.neon.NeonLanguage;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-
+import org.junit.Test;
+import org.junit.Assert;
 
 public class ParserTest extends ParsingTestCase {
+
 	public ParserTest() {
 		super("", "neon", new NeonParserDefinition());
 		PlatformTestCase.initPlatformLangPrefix();
-	}
-
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		// addExplicitExtension(LanguageASTFactory.INSTANCE, NeonLanguage.LANGUAGE, ?? );
 	}
 
 	@Override
@@ -29,35 +17,24 @@ public class ParserTest extends ParsingTestCase {
 		return "src/test/data";
 	}
 
-
-	public void testSample01() {
-		PsiFile file = createPsiFile("01.neon", getTestDataPath() + "/01.neon");
-		ensureParsed(file);
-
-		for(PsiElement el : file.getChildren()) {
-			System.out.println(el.getText());
+	protected void doTest(boolean checkResult, boolean suppressErrors) {
+		doTest(true);
+		if (!suppressErrors) {
+			Assert.assertFalse(
+				"PsiFile contains error elements",
+				toParseTreeText(myFile, true, includeRanges()).contains("PsiErrorElement")
+			);
 		}
 	}
 
-
-
-//
-//	public static void main(String[] args) {
-//		new ParserTest().testSample01();
-//	}
-
-	/*** helpers ***/
-
-	/**
-	 * Make sure a file gets parsed, by walking thru its structure
-	 */
-	private void ensureParsed(PsiFile file) {
-		// Ensure parsed
-		file.accept(new PsiElementVisitor() {
-			@Override
-			public void visitElement(PsiElement element) {
-				element.acceptChildren(this);
-			}
-		});
+	@Test
+	public void test01() {
+		doTest(true, false);
 	}
+
+	@Test
+	public void testError() {
+		doTest(true, false);
+	}
+
 }
