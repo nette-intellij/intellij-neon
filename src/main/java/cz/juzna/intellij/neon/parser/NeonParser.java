fa0b09017d2cf60fa720ea93b1277815997c04df
Jan Langer <jan.langer@gmail.com>
2014-08-23 23:57:36 +0200

    do not mark comma as error in inline array [closes #25]

--- src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
+++ src/main/java/cz/juzna/intellij/neon/parser/NeonParser.java
@@ -132,7 +132,7 @@ public class NeonParser implements PsiParser, NeonTokenTypes, NeonElementTypes {
 
 			}
 
-			if (myBuilder.getTokenType() == NEON_INDENT) advanceLexer();
+			if (myBuilder.getTokenType() == NEON_INDENT || (isInline && myBuilder.getTokenType() == NEON_ITEM_DELIMITER)) advanceLexer();
 		}
 
 //		marker.done(ARRAY);
