6d0e9ecdbad386e6ef32a8224c36416e2245e3cd
David Matejka <matej21@matej21.cz>
2016-01-10 17:20:29 +0100

    added GoToClassHandler

---
+++ src/main/java/cz/juzna/intellij/neon/reference/GoToClassHandler.java
@@ -0,0 +1,34 @@
+package cz.juzna.intellij.neon.reference;
+
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import cz.juzna.intellij.neon.psi.NeonScalar;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+
+public class GoToClassHandler implements GotoDeclarationHandler {
+
+	@Nullable
+	@Override
+	public PsiElement[] getGotoDeclarationTargets(@Nullable PsiElement element, int offset, Editor editor) {
+		if (element == null || element.getParent() == null || !(element.getParent() instanceof NeonScalar)) {
+			return new PsiElement[0];
+		}
+		PhpIndex phpIndex = PhpIndex.getInstance(element.getProject());
+		Collection<PhpClass> classes = phpIndex.getClassesByFQN(((NeonScalar) element.getParent()).getValueText());
+
+		return classes.toArray(new PsiElement[classes.size()]);
+	}
+
+	@Nullable
+	@Override
+	public String getActionText(DataContext context) {
+		return null;
+	}
+
+}
