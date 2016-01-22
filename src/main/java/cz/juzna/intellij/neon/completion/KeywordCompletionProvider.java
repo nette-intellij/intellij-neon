47c7cdc4cd4180607afdd3beae7dbe22e1bbe401
David Matejka <matej21@matej21.cz>
2016-01-22 23:20:17 +0100

    KeywordCompletionProvider: fixed incomplete key handling after a10836

--- src/main/java/cz/juzna/intellij/neon/completion/KeywordCompletionProvider.java
+++ src/main/java/cz/juzna/intellij/neon/completion/KeywordCompletionProvider.java
@@ -208,14 +208,14 @@ public class KeywordCompletionProvider extends CompletionProvider<CompletionPara
 		if (!NeonTokenTypes.STRING_LITERALS.contains(el.getNode().getElementType())) {
 			return false;
 		}
-		//error element
-		if (el.getParent() instanceof NeonArray
-				&& el.getPrevSibling() instanceof PsiErrorElement
-				&& ((PsiErrorElement) el.getPrevSibling()).getErrorDescription().equals(NeonParser.EXPECTED_ARRAY_ITEM)) {
-			return true;
-		}
 		//first scalar in file
 		if (el.getParent() instanceof NeonScalar && el.getParent().getParent() instanceof NeonFile) {
+			return true;
+		}
+		//new key after new line
+		if (el.getParent() instanceof NeonScalar
+				&& (el.getParent().getParent() instanceof NeonKeyValPair | el.getParent().getParent().getNode().getElementType() == NeonElementTypes.ITEM)
+				&& el.getParent().getPrevSibling().getNode().getElementType() == NeonTokenTypes.NEON_INDENT) {
 			return true;
 		}
 
