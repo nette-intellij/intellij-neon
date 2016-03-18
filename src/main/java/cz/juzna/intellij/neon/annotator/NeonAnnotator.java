acfabeac1707700a10f9bb01e90116e0b3a7612d
David Matejka <matej21@matej21.cz>
2016-03-18 16:31:35 +0100

    NeonAnnotator: added duplicate key error

--- src/main/java/cz/juzna/intellij/neon/annotator/NeonAnnotator.java
+++ src/main/java/cz/juzna/intellij/neon/annotator/NeonAnnotator.java
@@ -5,7 +5,13 @@ import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiErrorElement;
 import cz.juzna.intellij.neon.lexer.NeonTokenTypes;
+import cz.juzna.intellij.neon.psi.NeonArray;
+import cz.juzna.intellij.neon.psi.NeonKey;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class NeonAnnotator implements Annotator {
 
@@ -16,6 +22,16 @@ public class NeonAnnotator implements Annotator {
 				&& element.getPrevSibling() instanceof PsiErrorElement
 				&& ((PsiErrorElement) element.getPrevSibling()).getErrorDescription().equals("Tab/space mixing")) {
 			holder.createErrorAnnotation(element, "Tab/space mixing");
+		} else if (element instanceof NeonArray) {
+			List<NeonKey> arrayKeys = ((NeonArray) element).getKeys();
+			Set<String> keys = new HashSet<String>(arrayKeys.size());
+			for (NeonKey key : arrayKeys) {
+				if (keys.contains(key.getKeyText())) {
+					holder.createErrorAnnotation(key, "Duplicate key");
+				} else {
+					keys.add(key.getKeyText());
+				}
+			}
 		}
 	}
 }
