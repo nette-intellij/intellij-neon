3610ce218289d69523f7ca36ef0136d2b67b0617
mesour <matous.nemec@gmail.com>
2022-04-21 00:41:31 +0200

    Completed NeonPluginListener

--- src/main/java/cz/juzna/intellij/neon/completion/NeonCompletionContributor.java
+++ src/main/java/cz/juzna/intellij/neon/completion/NeonCompletionContributor.java
@@ -13,6 +13,5 @@ public class NeonCompletionContributor extends CompletionContributor {
 		extend(CompletionType.BASIC, StandardPatterns.instanceOf(PsiElement.class), new KeywordCompletionProvider());
 		extend(CompletionType.BASIC, StandardPatterns.instanceOf(PsiElement.class), new ServiceCompletionProvider());
 		extend(CompletionType.BASIC, StandardPatterns.instanceOf(PsiElement.class), new ClassCompletionProvider());
-		extend(CompletionType.BASIC, StandardPatterns.instanceOf(PsiElement.class), new NamespaceCompletionProvider());
 	}
 }
--- src/main/java/cz/juzna/intellij/neon/ide/NeonPluginListener.java
+++ src/main/java/cz/juzna/intellij/neon/ide/NeonPluginListener.java
@@ -8,12 +8,15 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ex.ApplicationUtil;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginsAdvertiser;
+import com.jetbrains.php.PhpIndex;
 import cz.juzna.intellij.neon.NeonBundle;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,16 +26,45 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashSet;
 
-
 public class NeonPluginListener implements ProjectManagerListener {
 
 	private static final @NotNull Logger LOG = Logger.getInstance(CommandProcessor.class);
 	private static final @NotNull String NOTIFICATION_GROUP = "NEON Pro Group";
 	private static final @NotNull String PLUGIN_PAGE_URL = "https://plugins.jetbrains.com/plugin/18387-neon-nette-support";
+	private static final @NotNull String DONATE_PAGE_URL = "https://github.com/sponsors/mesour";
 
 	@Override
 	public void projectOpened(@NotNull Project project) {
-		neonProSuggestion(NeonBundle.message("notification.neonProSuggestion.message"));
+		StartupManager.getInstance(project).registerPostStartupActivity(() -> new Thread(() -> {
+			try {
+				Thread.sleep(60000);
+
+				ApplicationUtil.tryRunReadAction(() -> {
+					PhpIndex phpIndex = PhpIndex.getInstance(project);
+					if (phpIndex.getClassesByFQN("Nette\\DI\\Container").size() > 0) {
+						neonProSuggestion(NeonBundle.message("notification.neonProSuggestion.message"));
+					} else {
+						neonDonateSuggestion(NeonBundle.message("notification.neonDonate.message"));
+					}
+					return null;
+				});
+
+			} catch (InterruptedException e) {
+				//do nothing
+			}
+		}).start());
+	}
+
+	public static void neonDonateSuggestion(final @NotNull String message) {
+		final Notification notification = new Notification(NOTIFICATION_GROUP, message, NotificationType.IDE_UPDATE);
+		notification.addAction(new NotificationAction(NeonBundle.message("notification.neonDonate.button.donate")) {
+			@Override
+			public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+				openUrl(DONATE_PAGE_URL);
+			}
+		});
+
+		showNotification(notification);
 	}
 
 	public static void neonProSuggestion(final @NotNull String message) {
@@ -44,7 +76,9 @@ public class NeonPluginListener implements ProjectManagerListener {
 					PluginId pluginId = PluginId.getId("com.mesour.intellij.neon");
 					PluginManagerCore.getPlugin(pluginId);
 					PluginsAdvertiser.installAndEnable(
+							null,
 							new HashSet<PluginId>() {{ add(pluginId); }},
+							true,
 							() -> PluginManager.disablePlugin("cz.juzna.intellij.neon")
 					);
 				}
@@ -52,7 +86,7 @@ public class NeonPluginListener implements ProjectManagerListener {
 			notification.addAction(new NotificationAction(NeonBundle.message("notification.neonProSuggestion.button.seeMore")) {
 				@Override
 				public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-					openUrl();
+					openUrl(PLUGIN_PAGE_URL);
 				}
 			});
 			final Application app = ApplicationManager.getApplication();
@@ -65,13 +99,20 @@ public class NeonPluginListener implements ProjectManagerListener {
 		}
 	}
 
-	private static void openUrl() {
+	private static void showNotification(final Notification notification) {
+		final Application app = ApplicationManager.getApplication();
+		if (!app.isDisposed()) {
+			app.getMessageBus().syncPublisher(Notifications.TOPIC).notify(notification);
+		}
+	}
+
+	private static void openUrl(final String url) {
 		if (Desktop.isDesktopSupported()) {
 			Desktop desktop = Desktop.getDesktop();
 
 			if (desktop.isSupported(Desktop.Action.BROWSE)) {
 				try {
-					URI uri = new URI(PLUGIN_PAGE_URL);
+					URI uri = new URI(url);
 					desktop.browse(uri);
 				} catch (URISyntaxException | IOException ignored) {
 				}
